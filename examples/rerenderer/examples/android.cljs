cd2a3b58cc2751c94d6c4bacaabfc83019971f07
nvbn <nvbn.rm@gmail.com>
2015-06-01 05:30:09 +0300

    Make android example more complex

--- examples/rerenderer/examples/android.cljs
+++ examples/rerenderer/examples/android.cljs
@@ -5,21 +5,30 @@
             [rerenderer.android :as a]))
 
 (defn root
-  [ctx {:keys [paint time]}]
+  [ctx {:keys [paint time cx cy]}]
   (r/.. ctx -canvas (drawRGB 250 0 0))
   (r/.. ctx -paint (setARGB 250 150 0 150))
   (r/.. ctx -paint (setTextSize 100))
   (r/.. ctx -canvas (drawText "It's working!" 100 1000 paint))
   (r/.. ctx -paint (setARGB 250 250 250 250))
   (r/.. ctx -paint (setTextSize 30))
-  (r/.. ctx -canvas (drawText (str time) 100 1500 paint)))
+  (r/.. ctx -canvas (drawText (str time) 100 1500 paint))
+  (r/.. ctx -paint (setARGB 150 0 250 0))
+  (r/.. ctx -canvas (drawCircle cx cy 100 paint)))
 
 (defn init!
   []
   (let [platform (a/android js/android)
         state (atom {:paint (.paint js/android)
-                     :time (js/Date.)})]
+                     :time (js/Date.)
+                     :cx 0
+                     :cy 0})]
     (r/init! platform state root)
+    (go-loop []
+      (swap! state update-in [:cx] #(-> % (+ 10) (mod 1080)))
+      (swap! state update-in [:cy] #(-> % (+ 10) (mod 1920)))
+      (<! (timeout 10))
+      (recur))
     (go-loop []
       (swap! state assoc :time (js/Date.))
       (<! (timeout 300))
